973d6da7cc41a7b1f7dc814f031e71439b2c64df
KrasimirVasilev <krasimirsv@abv.bg>
2015-01-14 22:00:10 +0200

    Start EightJokes project and add Parse

---
+++ app/src/main/java/com/neverland/eightjokes/Constants.java
@@ -0,0 +1,14 @@
+package com.neverland.eightjokes;
+
+/**
+ * Created by Vasilev on 14.1.2015 г..
+ */
+public class Constants {
+
+    //Parse
+    public static final String PARSE_APPLICATION_ID = "3SvaNt7yIZxdkOtaZRUZEJhQGNZV29itg1b6fweZ";
+    public static final String PARSE_CLIENT_KEY = "i5Ohn2mwv31QRhwJbGsJzI0hIwc2OGgNZJDXVqf0";
+
+    //TAG
+    public static final String TAG = "com.neverland.eightjokes.TAG";
+}
